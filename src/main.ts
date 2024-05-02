ded5fe2609564c36a93a50f69e528449783d0df2
pablo <pablocl09@gmail.com>
2024-05-02 13:55:18 +0200

    Cors enabled

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   // Congiguracion global de los pipes 
   app.useGlobalPipes(
     new ValidationPipe({
